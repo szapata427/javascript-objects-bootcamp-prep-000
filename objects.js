2d9c3f5a504b264eaa4d81b8de4eedb4d954e65d
Steven Zapata <szapata427@gmail.com>
2018-08-08 04:36:07 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,15 +1,4 @@
-var playlist = { artistName: "songTitle" };
 
-function updatePlaylist(playlist, Maluma, mala) {
-  
- return Object.assign({}, playlist, {[Maluma]: mala})
-}
-
-function removeFromPlaylist(playlist, Maluma) {
-  delete.playlist.Maluma;
-  
-  
-}
 
 
 var playlist = {};
