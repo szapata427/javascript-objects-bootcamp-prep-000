47e4112aedfcd3cc471b239fdf93e5dbb28ae0b2
Steven Zapata <szapata427@gmail.com>
2018-08-08 04:23:27 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,19 @@
+var playlist = { maluma: "corazon" };
+
+function updatePlaylist(obj, key, value) {
+  
+  playlist[reykon] = "mala"
+  
+  return playlist
+}
+
+
+function updatePlaylist(playlist, key, value) {
+  playlist[key] = value
+ 
+  return playlist
+}
+
+const playlist = { mauluma: corazon}
+
+updatePlaylist(playlist, 'reykon', 'mala')
